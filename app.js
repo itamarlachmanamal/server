eba2990df1f0a15e84e6e1936f4b1c7c40a3fdf0
itamar.lachman.amal <itamar.lachman.amal@gmail.com>
2025-12-09 13:29:36 +0200

    remove the db from the app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const PORT = 3000;
-
-const db = require('./middleware/mysqldb');
 const server = require('./middleware/server');
 
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running 
+                 on http://localhost:${PORT}`);
 });
 
 
